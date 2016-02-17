f7a710bf7bdc15d343db5a64d8b782bde25b6b7e
domzzzzzz <dom42486@gmail.com>
2016-02-17 13:13:09 -0800

    Updated Hi/Lo game

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,18 +1,39 @@
 class MainController < ApplicationController
 
-  def try
+  # def try
+  #   @your_num = params[:number]
+  #   @num = Random.rand(100)
+  #   session[:secretnum] = @num unless session[:secretnum].nil?
+  #   if params[:number].to_i < @num
+  #     @result_string = "too low"
+  #   elsif params[:number].to_i > @num
+  #     @result_string = "too high"
+  #   else
+  #     @result_string = "correct"
+  #   end # end if
+  #   render "try.html.erb"
+  #
+  # end # end of method
+
+  def answers
+    @secretnum = 45
+    @result_string
     @your_num = params[:number]
-    @num = Random.rand(100)
-    session[:secretnum] = @num unless session[:secretnum].nil?
-    if params[:number].to_i < @num
+    
+    if params.has_key?(:user_name) && !params[:user_name].strip.empty?
+      cookies[:name] = params[:user_name]
+    end
+    @user_name = cookies[:name]
+
+    if params[:number].to_i < @secretnum
       @result_string = "too low"
-    elsif params[:number].to_i > @num
+    elsif params[:number].to_i > @secretnum
       @result_string = "too high"
     else
-      @result_string = "correct"
-    end # end if
-    render "try.html.erb"
+      @result_string = "correctamundo"
+    end
 
-  end # end of method
+    render "try.html.erb"
+  end #end of method
 
 end #end of class
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/try/:number' => 'main#try'
+  get '/answers' => 'main#answers'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
